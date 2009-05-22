60968a85db03b70e8f53d491dab33d478563919b
jsmestad <justin.smestad@gmail.com>
2009-05-22 00:45:02 -0600

    app displays proper feed items, building ugly hash for the time being. some small js added for candy

--- lib/gripht/app.rb
+++ lib/gripht/app.rb
@@ -17,7 +17,20 @@ module Gripht
     
     get '/' do
       resource = RestClient::Resource.new 'http://www.pivotaltracker.com/services/v2/projects', :headers => { 'X-TrackerToken' => ENV['TRACKER_TOKEN']}
-      @projects = Nokogiri::XML.parse(resource.get).xpath('//project').collect { |r| { :id => r.at('id').content, :name => r.at('name').content } }
+      @projects = Nokogiri::XML(resource.get).xpath('//project').collect { |r| { :id => r.at('id').content, :name => r.at('name').content } }
+      @projects.each do |project|
+        project.merge!({ :stories => Nokogiri::XML(resource["#{project[:id]}/stories?limit=10&filter=state%3Astarted"].get).xpath('//story').collect do |story|
+                           {
+                             :name => story.at('name').content,
+                             :owner => story.at('owned_by').content,
+                             :created_at => story.at('created_at').content,
+                             :description => story.at('description').nil? ? "" : story.at('description').content,
+                             :url => story.at('url').content,
+                             :type => story.at('story_type').content
+                           }
+                         end
+                       })
+      end
       
       haml :index
     end
